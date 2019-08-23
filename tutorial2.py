1f47464a02c6df800c097f218fcb42906dfc97d8
Tahanie <Tahanie.alsanie@gmail.com>
2019-08-23 23:16:34 +0300

    Second Lesson

---
+++ tutorial2.py
@@ -0,0 +1,15 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #python comments
+>>> print("Hello, world") #Second lesson
+Hello, world
+>>> #print ("Hello, world")
+>>> print ("Cheers, Mate!")
+Cheers, Mate!
+>>> """ This is a comment
+written in more than one line """
+' This is a comment\nwritten in more than one line '
+>>> print ("Multiple line of comments")
+Multiple line of comments
+>>> 
